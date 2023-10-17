ac20740850d00d72c7b68e1e890ceebc5972c1a2
Rithvik Shankar <rithviks@gmail.com>
2023-10-17 21:32:27 +0530

    Added some styling and added functionality 1. Some styling 2. Disable button if input text is blank 3. Changed the html structure

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const addTask = () => {
-  let taskInput = document.getElementById('taskInput');
-  let taskList = document.getElementById('taskList');
+  let taskInput = document.getElementById('task-input');
+  let taskList = document.getElementById('task-list');
+  let addButton = document.querySelector('.add-button')
 
   let newTask = document.createElement('li');
   let taskText = document.createTextNode(taskInput.value);
@@ -8,4 +9,25 @@ const addTask = () => {
   newTask.appendChild(taskText);
   taskList.appendChild(newTask);
   taskInput.value = ''
+  addButton.disabled = true;
 }
+
+// To prevent users from making blank entries
+const toggleButton = () => {
+  let taskInput = document.getElementById('task-input');
+  let addButton = document.querySelector('.add-button');
+
+  if (taskInput.value.trim() !== '') {
+    addButton.disabled = false;
+  } else {
+    addButton.disabled = true;
+  }
+
+}
+
+// Adds the item to the list if the user clicks enter key
+document.getElementById('task-input').addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+    addTask();
+  }
+});
